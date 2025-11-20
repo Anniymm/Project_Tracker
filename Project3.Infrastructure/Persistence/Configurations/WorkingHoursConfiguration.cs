2f7a6b77a86590852650a226b6c36e71b8149d66
Anniymm <animaisuradze1999@gmail.com>
2025-11-20 13:34:37 +0400

    add WorkingHours entity configuration

--- Project3.Infrastructure/Persistence/Configurations/WorkingHoursConfiguration.cs
+++ Project3.Infrastructure/Persistence/Configurations/WorkingHoursConfiguration.cs
@@ -1,6 +1,44 @@
-﻿namespace Project3.Infrastructure.Persistence.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project3.Domain.Entities;
 
-public class WorkingHoursConfiguration
+namespace Project3.Infrastructure.Persistence.Configurations;
+
+public class WorkingHoursConfiguration : IEntityTypeConfiguration<WorkingHours>
 {
-    
+    public void Configure(EntityTypeBuilder<WorkingHours> builder)
+    {
+        builder.ToTable("working_hours");
+
+        
+        builder.HasKey(x => x.Id);
+
+        
+        builder.Property(x => x.Id)
+            .ValueGeneratedNever();
+
+        
+        builder.Property(x => x.ProviderId)
+            .IsRequired();
+
+        builder.HasOne<ServiceProviders>()
+            .WithMany()
+            .HasForeignKey(x => x.ProviderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(x => x.DayOfWeek)
+            .IsRequired();
+
+        builder.Property(x => x.StartTime)
+            .HasColumnName("start_time")
+            .IsRequired();
+
+        builder.Property(x => x.EndTime)
+            .HasColumnName("end_time")
+            .IsRequired();
+
+        builder.Property(x => x.IsActive)
+            .HasColumnName("is_active")
+            .IsRequired();
+    }
 }
