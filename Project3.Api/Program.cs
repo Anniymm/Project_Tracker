337a978391ad7db07f68b9fbcdfa82ba8bf3b5e0
Anniymm <animaisuradze1999@gmail.com>
2025-11-24 14:42:24 +0400

    add swagger configs

--- Project3.Api/Program.cs
+++ Project3.Api/Program.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Project3.Infrastructure.Persistence;
 
-
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 if (app.Environment.IsDevelopment())
 {
