a113e581605b7517e26ea317a078f6f341cd517a
Anniymm <animaisuradze1999@gmail.com>
2025-11-28 16:05:58 +0400

    add repository

---
+++ Project3.Application/Common/Interfaces/IBlockedTimesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Project3.Domain.Entities;
+
+namespace Project3.Application.Common.Interfaces;
+
+public interface IBlockedTimesRepository
+{
+    Task<BlockedTimes?> GetByIdAsync(Guid id);
+    
+    // radgan marto vfetchav da add-s an removes ar viyeneb, jobia IEnumerable an IReadOnlyList(indexing tu minda) 
+    Task<IEnumerable<BlockedTimes>> GetByProviderIdAsync(Guid providerId);
+    
+    Task<IEnumerable<BlockedTimes>> GetAllAsync();
+    
+    Task AddAsync(BlockedTimes blockedTimes);
+    
+    Task UpdateAsync(BlockedTimes blockedTimes);
+    
+    Task RemoveAsync(BlockedTimes blockedTimes);
+    
+}
