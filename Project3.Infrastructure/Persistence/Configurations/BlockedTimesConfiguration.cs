c94733af6731e3c6d977baa38f65f1533e7526a8
Anniymm <animaisuradze1999@gmail.com>
2025-11-28 14:30:20 +0400

    add Entity configurations

---
+++ Project3.Infrastructure/Persistence/Configurations/BlockedTimesConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project3.Domain.Entities;
+
+namespace Project3.Infrastructure.Persistence.Configurations
+{
+    public class BlockedTimesConfiguration : IEntityTypeConfiguration<BlockedTimes>
+    {
+        public void Configure(EntityTypeBuilder<BlockedTimes> builder)
+        {
+            builder.ToTable("blocked_times");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(x => x.ProviderId)
+                .IsRequired();
+
+            builder.Property(x => x.StartDateTime)
+                .IsRequired();
+
+            builder.Property(x => x.EndDateTime)
+                .IsRequired();
+
+            builder.Property(x => x.Reason)
+                .HasMaxLength(500);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.HasOne(x => x.ServiceProvider)
+                .WithMany()
+                .HasForeignKey(x => x.ProviderId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Project3.Infrastructure/Persistence/Configurations/NotificationLogsConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project3.Domain.Entities;
+
+namespace Project3.Infrastructure.Persistence.Configurations
+{
+    public class NotificationLogsConfiguration : IEntityTypeConfiguration<NotificationLogs>
+    {
+        public void Configure(EntityTypeBuilder<NotificationLogs> builder)
+        {
+            builder.ToTable("notification_logs");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(x => x.AppointmentId)
+                .IsRequired();
+
+            builder.Property(x => x.Type)
+                .IsRequired();
+
+            builder.Property(x => x.Status)
+                .IsRequired();
+
+            builder.Property(x => x.SentAt)
+                .IsRequired();
+            
+            builder.HasOne(x => x.Appointment)
+                .WithMany()
+                .HasForeignKey(x => x.AppointmentId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
